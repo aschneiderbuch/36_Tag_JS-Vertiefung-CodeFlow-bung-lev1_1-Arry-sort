317cda0e3187530309c475083822b108757b7a87
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-01 14:14:34 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
 console.log('hello world');
+
+let languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+  ];
+
+  
+
+  console.log(languages);
+  console.table(languages);
+  document.write(languages+ `<br>`);
+
+  function Sortierung(){
+languages.sort();
+
+
+    console.log(languages);
+    console.table(languages);
+    document.write(languages+ `<br>`);
+  }
+
+
+  Sortierung();
+
